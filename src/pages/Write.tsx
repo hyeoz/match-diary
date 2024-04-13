195e75d1ae5720eb0eb453a04c147a07a4e8b4f3
hyeoz <jenny970203@gmail.com>
2024-04-13 22:15:01 +0900

    feat: bottom tab touchable wrapper 컴포넌트 분리

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,20 +1,27 @@
 import {
+  Alert,
   SafeAreaView,
   StyleSheet,
   Text,
-  TouchableWithoutFeedback,
+  TouchableOpacity,
 } from 'react-native';
-import useBottomTabState from '../store/default';
+
+import TouchableWrapper from '../components/TouchableWrapper';
 
 function Write() {
-  const {isOpen, update} = useBottomTabState();
   // TODO 화면 터치 감지를 통해 바텀탭 플로팅을 숨기기 위해 SafeAreaView 를 TouchableWithoutFeedback 으로 감싸도 되는지?
   return (
-    <TouchableWithoutFeedback onPress={() => isOpen && update()}>
+    <TouchableWrapper>
       <SafeAreaView style={styles.pageWrapper}>
         <Text>Write</Text>
+        <TouchableOpacity
+          onPress={() => {
+            Alert.alert('BUTTON WORKS?');
+          }}>
+          <Text>BUTTON</Text>
+        </TouchableOpacity>
       </SafeAreaView>
-    </TouchableWithoutFeedback>
+    </TouchableWrapper>
   );
 }
 
@@ -24,4 +31,5 @@ const styles = StyleSheet.create({
   },
 });
 
+// export default withTouchable(<Write />);
 export default Write;
