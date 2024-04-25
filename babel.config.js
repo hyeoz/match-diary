dbdbd970150d6044c1f082de1138aeda4c90c221
hyeoz <jenny970203@gmail.com>
2024-04-25 17:40:41 +0900

    add: babel 절대경로 설정

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,29 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-export-namespace-from',
     'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@assets': './src/assets',
+          '@svg': './src/assets/svg',
+          '@components': './src/components',
+          '@oages': './src/pages/',
+        },
+      },
+    ],
   ],
 };
