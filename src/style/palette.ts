ff2b57d2fe118112f0f1d3bb9d3d635b542a9b34
hyeoz <jenny970203@gmail.com>
2024-04-26 09:41:15 +0900

    styles: 팀 컬러 추가

--- src/style/palette.ts
+++ src/style/palette.ts
@@ -1,5 +1,14 @@
-const teamColor = {
+const teamColor: { [key: string]: string } = {
   ssg: '#BF282C',
+  nc: '#385185',
+  ds: '#12122d',
+  kt: '#000000',
+  lg: '#b32653',
+  hr: '#781527',
+  kia: '#d82f35',
+  lt: '#0d2751',
+  ss: '#204b9b',
+  hh: '#ed702d',
 };
 
 const commonColor = {
@@ -8,4 +17,17 @@ const commonColor = {
   yellow: '#feff9c',
 };
 
-export const palette = { teamColor, commonColor };
+const greyColor = {
+  gray1: '#111',
+  gray2: '#222',
+  gray3: '#333',
+  gray4: '#444',
+  gray5: '#555',
+  gray6: '#666',
+  gray7: '#777',
+  gray8: '#888',
+  gray9: '#999',
+  border: '#c8c8c8',
+};
+
+export const palette = { teamColor, commonColor, greyColor };
