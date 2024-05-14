368df8cea352f42126c914e68e86ab8f0b809474
hyeoz <jenny970203@gmail.com>
2024-05-14 17:49:14 +0900

    feat: 네이버 지도 설치 중

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,10 +1,12 @@
 import { Text } from 'react-native';
 
 import TouchableWrapper from '@components/TouchableWrapper';
+import NaverMapView from 'react-native-nmap';
 
 function Discover() {
   return (
     <TouchableWrapper>
+      <NaverMapView style={{ width: '100%', height: '100%' }} />
       <Text>Discover</Text>
     </TouchableWrapper>
   );
