69050ced0122dadb6351520ead8208e1052eb0c3
hyeoz <jenny970203@gmail.com>
2024-05-21 16:57:04 +0900

    rename: app name

--- android/app/src/main/java/com/matchdiary/MainActivity.kt
+++ android/app/src/main/java/com/matchdiary/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "matchdiary"
+  override fun getMainComponentName(): String = "직관일기"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
