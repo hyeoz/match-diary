c650613ae7858d9fec7f7418453a191a8c9ba7c0
hyeoz <jenny970203@gmail.com>
2024-05-21 18:01:35 +0900

    feat: ios 위치정보 permission 작업 중

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@
 
 import React, { useEffect, useRef } from 'react';
 import {
+  Alert,
   Platform,
   SafeAreaView,
   StyleSheet,
@@ -18,7 +19,13 @@ import {
   NavigationContainer,
   useNavigationContainerRef,
 } from '@react-navigation/native';
-import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
+import {
+  check,
+  request,
+  PERMISSIONS,
+  RESULTS,
+  openSettings,
+} from 'react-native-permissions';
 
 import Router from './src/router';
 import Toast from 'react-native-toast-message';
@@ -35,20 +42,29 @@ function App(): React.JSX.Element {
     const requestLocationPermission = async () => {
       if (Platform.OS === 'ios') {
         const status = await check(PERMISSIONS.IOS.LOCATION_ALWAYS);
-        if (status !== RESULTS.GRANTED) {
-          const statusWhenInUse = await request(
-            PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
-          );
-          if (statusWhenInUse === RESULTS.GRANTED) {
-            // 위치 "항상" 권한 요청
-            const statusAlways = await request(PERMISSIONS.IOS.LOCATION_ALWAYS);
-            if (statusAlways !== RESULTS.GRANTED) {
-              console.log('위치 "항상" 권한이 거부되었습니다.');
-            }
-          } else {
-            console.log('위치 "앱을 사용하는 동안" 권한이 거부되었습니다.');
-          }
-        }
+        // if (status === RESULTS.DENIED) {
+        // } else if (status !== RESULTS.GRANTED) {
+        // const statusWhenInUse = await request(
+        //   PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+        // );
+        // if (statusWhenInUse === RESULTS.GRANTED) {
+        // 위치 "항상" 권한 요청
+        // const statusAlways = await request(PERMISSIONS.IOS.LOCATION_ALWAYS);
+        // if (statusAlways !== RESULTS.GRANTED) {
+        //   console.log('위치 "항상" 권한이 거부되었습니다.');
+        // }
+        // } else {
+        //   Alert.alert(
+        //     '위치 서비스 비활성화',
+        //     '위치 서비스가 비활성화되어 있습니다. 활성화하려면 설정으로 이동하십시오.',
+        //     [
+        //       { text: '취소', style: 'cancel' },
+        //       { text: '설정으로 이동', onPress: () => openSettings() },
+        //     ],
+        //   );
+        //   console.log('위치 "앱을 사용하는 동안" 권한이 거부되었습니다.');
+        // }
+        // }
       }
     };
 
--- ios/matchdiaryTests/matchdiaryTests.m
+++ ios/matchdiaryTests/matchdiaryTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface 직관일기Tests : XCTestCase
+@interface matchdiaryTests : XCTestCase
 
 @end
 
-@implementation 직관일기Tests
+@implementation matchdiaryTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
