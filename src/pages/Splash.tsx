c6c7126432a2e7120cf6e7147c02ded96e253ac7
hyeoz <jenny970203@gmail.com>
2025-02-24 22:12:56 +0900

    fix: Splash 디폴트 컬러 수정 및 teams 호출

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -8,19 +8,23 @@ import { API } from '@/api';
 import { RootStackListType } from '@/type/default';
 import { useUserState } from '@/stores/user';
 import { palette } from '@style/palette';
+import { useTeamsState } from '@/stores/teams';
+
 import splash_text from '@assets/splash_text.png';
 
 function Splash({ navigation }: NativeStackScreenProps<RootStackListType>) {
   const { teamId } = useUserState();
-  const [defaultTeam, setDefaultTeam] = useState(teamId);
+  const { setTeams } = useTeamsState();
+  const [defaultTeam, setDefaultTeam] = useState(1);
 
   useEffect(() => {
     // TODO 스플래시 화면 / 유저 정보 확인 순서 정리하기
-    getAll();
-  });
+    getAllData();
+  }, []);
 
-  const getAll = async () => {
+  const getAllData = async () => {
     const deviceId = await getUniqueId();
+    await getTeamsData();
     await getUserData(deviceId);
   };
 
@@ -35,12 +39,19 @@ function Splash({ navigation }: NativeStackScreenProps<RootStackListType>) {
   // 기기정보로 서버에서 유저 데이터 불러오기
   const getUserData = async (deviceId: string) => {
     const res = await API.post('/user', { userId: deviceId });
-    if (!res.data) {
+    if (!res.data.length) {
       // 유저 정보가 없는 경우 가입 화면으로 넘기기
       await setReplace(false);
     } else {
       setDefaultTeam(res.data.teamId);
       await setReplace();
+    }
+  };
+
+  const getTeamsData = async () => {
+    const res = await API.get('/teams');
+    if (res.data) {
+      setTeams(res.data);
     }
   };
 
