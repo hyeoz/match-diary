e17413f3345e57cd21fd44caf0a06fe3e58bd60f
hyeoz <jenny970203@gmail.com>
2025-03-18 18:56:03 +0900

    fix: build error

--- ios/matchdiary/AppDelegate.h
+++ ios/matchdiary/AppDelegate.h
@@ -1,6 +1,7 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h> // 추가
 
-@interface AppDelegate : RCTAppDelegate
+@interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate> // 프로토콜 추가
 
 @end
