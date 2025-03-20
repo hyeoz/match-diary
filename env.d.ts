3a7068976cd5c36b57b101baa6fa6a4b28ed19f4
hyeoz <jenny970203@gmail.com>
2025-03-20 12:53:50 +0900

    add: admob 을 위한 라이브러리 및 id 추가

--- env.d.ts
+++ env.d.ts
@@ -5,4 +5,7 @@ declare module '@env' {
   export const REACT_APP_NAVER_CLIENT_SECRET_KEY: string;
   export const REACT_APP_HEROKU_API_KEY: string;
   export const REACT_APP_EC2_URI: string;
+  export const REACT_APP_ANDROID_APP_ID: string;
+  export const REACT_APP_IOS_APP_ID: string;
+  
 }
