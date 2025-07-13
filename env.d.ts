9a28fac7c659f0c385971b4df4d37b912f62b8ef
hyeoz <jenny970203@gmail.com>
2025-07-13 11:58:54 +0900

    add: gemini 세팅 추가

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,7 @@
 declare module '@env' {
   export const REACT_APP_EC2_URI: string;
+  export const REACT_APP_WEATHER_URI: string;
+  export const REACT_APP_GEMINI_URI: string;
   export const REACT_APP_DEV_UNIQUE_ID: string;
   export const REACT_APP_ANDROID_APP_ID: string;
   export const REACT_APP_IOS_APP_ID: string;
@@ -15,4 +17,5 @@ declare module '@env' {
   export const REACT_APP_IOS_MAP_BANNER_ID: string;
   export const REACT_APP_NAVER_MAP_CLIENT_ID: string;
   export const REACT_APP_NAVER_MAP_CLIENT_SECRET: string;
+  export const REACT_APP_GEMINI_API_KEY: string;
 }
